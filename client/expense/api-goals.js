991d13c0c197405be1b2ca9587f68d62e81cca20
Raviteja582 <raviteja1028312@gmail.com>
2024-11-29 21:38:21 -0500

    Implemented goals APIs in React

---
+++ client/expense/api-goals.js
@@ -0,0 +1,72 @@
+const createGoal = async (credentials, goal) => {
+  try {
+    console.log("[frontend] making API request");
+    let response = await fetch("/api/goals/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify(goal),
+    });
+    const result = await response.json();
+    console.log("[frontend] returned result: ", result);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateGoal = async (params, credentials, goal) => {
+  try {
+    let response = await fetch("/api/goals/" + params.goalId, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify(goal),
+    });
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const removeGoal = async (params, credentials) => {
+  try {
+    let response = await fetch("/api/goals/" + params.goalId, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllGoals = async (credentials) => {
+  try {
+    let response = await fetch("/api/goals/", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { createGoal, updateGoal, removeGoal, getAllGoals };
